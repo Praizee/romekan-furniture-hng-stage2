d239e08c7691d06aa6d01aacc4a66c1aec9c2b79
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-07-05 12:37:14 +0100

    cart and checkout pages

---
+++ app/(homepage)/cart/page.tsx
@@ -0,0 +1,14 @@
+import Cart from "@/app/components/Cart/Cart";
+import React from "react";
+
+export const metadata = {
+  title: "Your Shopping Cart",
+  description:
+    "Review your selected furniture items in your cart at Romekan Furnitures. Ensure you have everything you need before proceeding to checkout. Shop quality furniture today!",
+};
+
+const page = () => {
+  return <Cart />;
+};
+
+export default page;
---
+++ app/(homepage)/checkout/page.tsx
@@ -0,0 +1,14 @@
+import Checkout from "@/app/components/Checkout/Checkout";
+import React from "react";
+
+export const metadata = {
+  title: "Checkout",
+  description:
+    "Complete your purchase at Romekan Furnitures. Secure and fast checkout process. Buy your dream furniture with confidence and convenience. Shop now!",
+};
+
+const page = () => {
+  return <Checkout />;
+};
+
+export default page;
