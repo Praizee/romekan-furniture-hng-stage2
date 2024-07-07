a300d819bc217022bf55209b9446144f78de6d46
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-07-07 17:58:42 +0100

    feat: page animation

--- app/_layouts/BaseLayout.tsx
+++ app/_layouts/BaseLayout.tsx
@@ -1,13 +1,29 @@
+"use client";
+
+import { useEffect } from "react";
+import { motion, useAnimation } from "framer-motion";
+
 import { Footer, Navbar, ScrollButton } from "@/app/components/common";
 import type { BaseLayoutProps } from "@/app/interfaces";
 
 export const BaseLayout = ({ children }: BaseLayoutProps) => {
+  const controls = useAnimation();
+
+  useEffect(() => {
+    controls.start({
+      opacity: 1,
+      transition: { duration: 0.5 },
+    });
+  }, [controls]);
+
   return (
-    <section className="bg-white min-h-screen">
-      <Navbar />
-      {children}
-      <ScrollButton />
-      <Footer />
-    </section>
+    <motion.div initial={{ opacity: 0 }} animate={controls}>
+      <section className="bg-white min-h-screen">
+        <Navbar />
+        {children}
+        <ScrollButton />
+        <Footer />
+      </section>
+    </motion.div>
   );
 };
