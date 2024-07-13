c8b0e3321c4c6dd9eb105143249d19259f30994d
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-07-13 16:57:25 +0100

    Create cartStore.ts

---
+++ app/store/cartStore.ts
@@ -0,0 +1,51 @@
+import { create } from "zustand";
+
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  discountPrice: number;
+  imageSrc: string;
+  discount: string;
+}
+
+interface CartItem extends Product {
+  quantity: number;
+}
+
+interface CartStore {
+  cart: CartItem[];
+  addToCart: (product: Product) => void;
+  removeFromCart: (id: number) => void;
+  updateQuantity: (id: number, quantity: number) => void;
+  clearCart: () => void;
+}
+
+export const useCartStore = create<CartStore>((set) => ({
+  cart: [],
+  addToCart: (product) =>
+    set((state) => {
+      const existingItem = state.cart.find((item) => item.id === product.id);
+      if (existingItem) {
+        return {
+          cart: state.cart.map((item) =>
+            item.id === product.id
+              ? { ...item, quantity: item.quantity + 1 }
+              : item
+          ),
+        };
+      }
+      return { cart: [...state.cart, { ...product, quantity: 1 }] };
+    }),
+  removeFromCart: (id) =>
+    set((state) => ({
+      cart: state.cart.filter((item) => item.id !== id),
+    })),
+  updateQuantity: (id, quantity) =>
+    set((state) => ({
+      cart: state.cart.map((item) =>
+        item.id === id ? { ...item, quantity } : item
+      ),
+    })),
+  clearCart: () => set({ cart: [] }),
+}));
