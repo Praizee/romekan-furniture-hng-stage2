49bb20927f2a986c6c6bd313456174818c222f46
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-07-15 22:47:46 +0100

    product

---
+++ app/(homepage)/product/[...id]/page.tsx
@@ -0,0 +1,13 @@
+import Product from "@/app/components/Product/Product";
+import React from "react";
+
+export const metadata = {
+  title: "Product 1",
+  description: "Shop quality furniture today!",
+};
+
+const page = () => {
+  return <Product />;
+};
+
+export default page;
